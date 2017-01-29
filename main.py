11c8a03dfd6436a5ac60729f1f7edb26e21a053d
jzc <985811440@qq.com>
2017-01-29 09:35:58 +0800

    add some description

--- main.py
+++ main.py
@@ -16,17 +16,24 @@ def open1(file, mode, *args, **kwargs):
         file = os.path.join(PATH, os.path.basename(file))
         return _open(file, mode[:-1], *args, **kwargs)
 
-
 def load_cookies(filename):
-    with open1(filename, 'r&') as f:
-        data = json.load(f)
-    cookies = dict()
-    for item in data:
-        cookies[item["name"]] = item["value"]
-    return cookies
+    if filename.endswith(".json"):
+        with open1(filename, 'r&') as f:
+            data = json.load(f)
+        cookies = dict()
+        for item in data:
+            cookies[item["name"]] = item["value"]
+        return cookies
+    elif filename.endswith(".cookie"):
+        with open1(filename, 'r&') as f:
+            data = json.load(f)
+        return data
+    else:
+        pass
+
 
 def load_files(dirpath):
-    files_name = [x for x in os.listdir(dirpath) if x.endswith(".json")]
+    files_name = [x for x in os.listdir(dirpath) if x.endswith(".json") or x.endswith(".cookie")]
     users = list()
     for file in files_name:
         users.append(Tieba(load_cookies(file)))
@@ -37,8 +44,10 @@ def main():
     print("\r\n")
     users = load_files(PATH)
     for user in users:
+        print(user.username, "Signing")
         for name in user.get_likes():
             if user.sign_Wap(name):
                 time.sleep(10)
+        print("\r\n")
 
 main()
