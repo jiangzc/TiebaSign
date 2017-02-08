313716b68bdd5cc74661ce1e88ff3f1bb9e496e5
jzc <985811440@qq.com>
2017-02-08 20:18:04 +0800

    Use Wap to get liked tieba.

--- Tieba.py
+++ Tieba.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import Baidu
-import requests
 import re
-import json
-import urllib.request
+
+
+def Retry(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs) or func(*args, **kwargs) or func(*args, **kwargs)
+    return wrapper
+
 
 class Tieba(Baidu.Baidu):
     def _get_tbs(self, url):
@@ -14,54 +18,20 @@ class Tieba(Baidu.Baidu):
         else:
             return ""
 
-    def _set_stoken(self):
-        likes_url = "https://passport.baidu.com/v3/login/api/auth/?tpl=tb&jump=&return_type=3&u=http%3A%2F%2Ftie" \
-                    "ba.baidu.com%2Ff%2Flike%2Fmylike%3Fpn%3D1"
-        res = self.session.get(likes_url, allow_redirects=False)
-        likes_url = res.headers['Location']
-        res = self.session.get(likes_url, allow_redirects=False)
-        stoken = re.search("STOKEN=(.+?);", res.headers['Set-Cookie']).group(1)
-        self.session.cookies["STOKEN"] = stoken
-
     def _check_sign(self, tieba_name):
         res = self.session.get("http://tieba.baidu.com/mo/m?kw=" + tieba_name)
         return "已签到" in res.text
 
+    @Retry
     def get_likes(self):
-        #self._set_stoken()
-        res = self.session.get('http://tieba.baidu.com/f/like/mylike?pn=1', allow_redirects=False)
-        last = re.search("pn=(\d+)\">尾页</a>", res.text).group(1)
-        likes_url = 'http://tieba.baidu.com/f/like/mylike?pn='
-        likes_tieba = []
-        for i in range(1, int(last) + 1):
-            res = self.session.get(likes_url + str(i))
-            likes_tieba += re.compile('<a href.+?title="(.+?)"').findall(res.text)
-        return likes_tieba
-
-    def sign_PC(self, tieba_name):
-        tieba_url = "http://tieba.baidu.com/f?kw={0}&fr=index".format(tieba_name)
-        if self._check_sign(tieba_url):
-            print("Already signed in", tieba_name)
-            return False
-        tbs = self._get_tbs(tieba_url)
-        if not tbs:
-            print("Failed to grasp ", tieba_name, " !")
-            return False
-        sign_url = "http://tieba.baidu.com/sign/add"
-        response = self.session.post(
-            sign_url,
-            data={
-                'ie': 'utf-8',
-                'kw': tieba_name,
-                'tbs': tbs,
-            },
-        )
-        data = json.loads(response.text)
-        if data['no'] == 0:
-            print("Signed successfully in", tieba_name)
-            return True
-        else:
-            print("Failed to sign, reason:", data['error'], tieba_name)
+        try:
+            res = self.session.get("http://tieba.baidu.com/mo/")
+            likes_url = "http://tieba.baidu.com" + \
+                        re.search('"([^"]+tab=favorite)"', res.text).group(1).replace("&amp;", "&")
+            res = self.session.get(likes_url)
+            likes = re.findall('kw.+?">(.+?)<', res.text)
+            return likes
+        except:
             return False
 
     def sign_Wap(self, tieba_name):
@@ -87,7 +57,7 @@ class Tieba(Baidu.Baidu):
         if 'p' in str(tid):
             # tid is an URL
             url = tid
-            tid = re.search("\d+",url).group(0)
+            tid = re.search("\d+", url).group(0)
         else:
             # tid is a string of numbers
             url = "http://tieba.baidu.com/p/" + str(tid)
@@ -114,7 +84,7 @@ class Tieba(Baidu.Baidu):
         fid = re.search("fid: (\d+)", self.session.get(url).text).group(1)
         res = self.session.post(
             "http://tieba.baidu.com/f/commit/thread/add",
-            data = {
+            data={
                 "ie": "utf-8",
                 "kw": tieba_name,
                 "fid": fid,
@@ -128,4 +98,3 @@ class Tieba(Baidu.Baidu):
                 "__type__": "thread"
             })
         print(res.text)
-
